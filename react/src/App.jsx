0ec8e8758a56e45ba6e0ab33012925144a556706
Adam Smith <adamjsmith002@gmail.com>
2025-06-26 11:52:05 -0400

    lab 5

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,9 +2,13 @@ import Sock from "./components/Sock";
 import sock_data from './assets/sock.json';
 import Footer from "./components/Footer";
 import Search from "./components/Search"
-import FeaturedContainer from "./components/FeaturedContainer";
+import Featured from "./components/Featured";
 import promo_data from "./assets/promo.json"
 import React, { useState, useEffect } from "react";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import About from "./components/About"
+import AddSock from "./components/AddSock"
 
 
 function App() {
@@ -49,7 +53,7 @@ function App() {
   
   
   return (
-    <>
+    <Router>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
@@ -70,14 +74,21 @@ navigation"
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
+              <Link className="nav-link" to="/about">
+                About
+              </Link>;
+
+              </li>
+              <li className="nav-item">
+              <Link className="nav-link" to="/addSock">
+                Add Sock
+              </Link>;
+
               </li>
               <li className="nav-item dropdown">
                 <a
@@ -126,22 +137,25 @@ navigation"
           <div className="row">
             Both socks and space rockets will take you to new heights, but only
             one will get cold feet!
-            < FeaturedContainer props={promo_data}  />
+            < Featured props={promo_data}  />
             <div className="card-container" style={{ display: 'flex', flexWrap: 'wrap', gap:'20px' }}>
-              {
-               data.map((sock) => (
-                <Sock key={sock._id} data={sock} handleDelete={handleDelete} />
-                ))
-              }
-                
-              
-
             </div>
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={<Home data={data} handleDelete={handleDelete} />}
+              />
+              <Route path="/about" element={<About />} />
+              <Route path="/addSock" element={<AddSock />} />
+              
+            </Routes>;
+
             <Footer environment={import.meta.env.VITE_ENVIRONMENT} />
           </div>
         </div>
       </main>
-    </>
+    </Router>
   );
 }
 export default App;
--- react/src/components/Featured.jsx
+++ react/src/components/Featured.jsx
@@ -1,9 +1,10 @@
 import FeaturedCard from "./FeaturedCard"
 
-const FeaturedContainer = (props) => {
+const Featured = (props) => {
     return (
             <div className="card-container d-flex flex-row justify-content-start" style={{
             gap: "20px", padding: "20px" }}>
+                <h2>Featured</h2>
             {
                 props.props.map((item) => (
                     <FeaturedCard  key={item.id}  data={item.feature} />
@@ -14,4 +15,4 @@ const FeaturedContainer = (props) => {
     )
 }
 
-export default FeaturedContainer;
+export default Featured;
